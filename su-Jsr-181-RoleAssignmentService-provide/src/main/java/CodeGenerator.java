b0ff188c5e43d677539efff4ec3b1233ca1d8435
Thiago Colucci <ticolucci@gmail.com>
2011-05-25 18:37:53 -0300

    Fixing wsdl to use with axis 2

--- su-Jsr-181-RoleAssignmentService-provide/src/main/java/CodeGenerator.java
+++ su-Jsr-181-RoleAssignmentService-provide/src/main/java/CodeGenerator.java
@@ -1,10 +1,19 @@
-import org.apache.cxf.tools.wsdlto.WSDLToJava;
+import org.apache.axis2.wsdl.WSDL2Java;
+
 
 public class CodeGenerator {
-        public static void main(String[] args) {
-                WSDLToJava.main(new String[] { "-server", "-d",
-                                "src/main/java",
-                                "src/main/resources/RoleAssignmentService.wsdl" });
-                System.out.println("Done!");
-        }
+    public static void main(String[] args) throws Exception {
+        WSDL2Java.main(new String[] {
+                       "-d", "src/main/java",
+                        "-p", "br.usp.ime.ccsl.choreos.middleware.spike",
+                        "-s", 
+                        "-wv", "1.1",
+                        "-ss",
+                        "-sd",
+                        "-ssi",
+                        "-uri", "src/main/resources/RoleAssignmentService.wsdl"
+                        });
+        System.out.println("Done!");
+    }
+
 }
